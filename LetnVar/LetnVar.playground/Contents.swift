fcc25e624ed9684d99c6dd15fe3196d2b247781e
ape0108792 <ape0108792@naver.com>
2017-09-19 10:12:51 +0900

    back

--- LetnVar/LetnVar.playground/Contents.swift
+++ LetnVar/LetnVar.playground/Contents.swift
@@ -3,15 +3,3 @@
 import UIKit
 
 var str = "Hello, playground"
-
-
-/* let은 기본적으로 변경이 불가능한 상수를 만들고, var는 변경이 가능한 변수를 만든다 */
-
-let name = "Hwangdojeung"
-
-var weight:Int = 60
-
-//weight = 40.5
-//weight = "70"
-//var는 변경가능한 변수이지만, 처음 선언한 변수타입에 맞는 변수로만 변경가능하다.
-
